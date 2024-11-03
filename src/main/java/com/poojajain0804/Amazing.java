4249d9fcc3ad7bbc4e4effe9c4ae8563b55d94df
poojajain0804 <pooja.jain.0804@gmail.com>
2024-11-04 00:26:09 +0530

    annotation based configuration
    config class based configuration

--- src/main/java/com/poojajain0804/Amazing.java
+++ src/main/java/com/poojajain0804/Amazing.java
@@ -1,4 +1,5 @@
 package com.poojajain0804;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Amazing {
@@ -6,17 +7,7 @@ public class Amazing {
 	public static void main(String[] args) {
 		System.out.println("Hi, this is an amazing world");
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-		
-		/*
-		 * Vehical vehicalBean = context.getBean("busBean", Vehical.class);
-		 * System.out.println(vehicalBean.getMileage());
-		 * System.out.println(vehicalBean.getDiscountOffer());
-		 * 
-		 * vehicalBean = context.getBean("carBean", Vehical.class);
-		 * System.out.println(vehicalBean.getMileage());
-		 * System.out.println(vehicalBean.getDiscountOffer());
-		 */
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 		
 		
 		System.out.println("Now lets play with annotation");
---
+++ src/main/java/com/poojajain0804/Config.java
@@ -0,0 +1,10 @@
+package com.poojajain0804;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.poojajain0804")
+public class Config {
+
+}
